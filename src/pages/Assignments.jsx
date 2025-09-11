3aa65b8ceb2b7045f4634c47e4a425c88aabce89
xiaojianggis <lixiaojiang.gis@gmail.com>
2025-09-11 13:32:34 -0400

    update

--- src/pages/Assignments.jsx
+++ src/pages/Assignments.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import ReactMarkdown from "react-markdown";
 import "./Assignments.css";
 
+
 const assignments = [
   {
     id: "a1",
@@ -37,8 +38,64 @@ const assignments = [
 
 ### Submission Notes
 - Submit via **Canvas**. Please submit your notebook to Canvas directly, not as a GitHub link.
+    `
+  },
 
 
+{
+    id: "a2",
+    title: "HW #2 — Exploratory Data Visualization",
+    assigned: "Thu, Sep 11",
+    due: "Sun, Sep 21 @ 11:59 PM",
+    detailsMd: `
+**Assigned on:** Thu, September 11 
+**Due:** Sun, September 21 at 11:59 PM  
+
+---
+### Part 1: Part 1: Select a dataset
+
+In this part, you’ll use matplotlib, seaborn, and altair to explore a dataset of your choosing and generate some charts in a Jupyter notebook.
+
+
+For this assignment, you can choose your own dataset to explore. I recommend selecting a dataset from OpenDataPhilly. You are welcome to to use a dataset from elsewhere, but please email your instructor and let them know what you want to analyze.
+
+Datasets with timestamped entries will be particularly good for analysis, but there are many interesting datasets to consider. They include:
+
+- [311 Requests](https://www.opendataphilly.org/dataset/311-service-and-information-requests)
+- [Voter turnout](https://opendataphilly.org/datasets/voter-turnout/)
+- [Parking violations](https://opendataphilly.org/datasets/parking-violations/)
+- [Shooting victims](https://opendataphilly.org/datasets/shooting-victims/)
+- and many more…
+
+
+### Part 2: Explore and visualize the data
+
+From within a Jupyter notebook, you should explore the datasets and generate charts visualizing different aspects of the data.
+
+Requirements:
+
+- **1 Matplotlib chart** (of any type)
+You should consider what aspect of the dataset might be best plotted using matplotlib.
+Include your reasoning for using matplotlib to visualize this specific aspect of the data.
+You will be graded on the aesthetics of the plot, namely, color choices and clarity.
+
+- **1 seaborn chart** (of any type)
+Please include the motivation behind your choice for the type of seaborn plot used and why.
+
+- **3 altair plots**
+Both of the following techniques should be used:
+  - A transformation (mean, count, binning, etc)
+  - Brush selection
+Include a short discussion (a few sentences) of the main conclusion of each chart (in a markdown cell below each chart). It does not need to be interesting or insightful, but it is good practice to always note the main conclusions so the notebook make sense after time passes.
+
+- **Extra credit** 2-chart altair dashboard,
+This should be in addition to the 3 charts above
+Filtering on one chart should cross-filter the other chart (via transform_filter())
+Note: You may include geospatial charts to satisfy the above requirements, but you are not required to.
+
+
+### Submission Notes
+- Submit via **Canvas**. Please submit your notebook and you data to Canvas directly using relative path.
     `
   }
 ];
